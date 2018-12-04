461816edbdfab6a943fc3aeabd7eca503a03e2bd
Dan Miller <danjm.com@gmail.com>
2018-12-04 13:23:32 -0330

    Run one assertion per test and properly handle txs which are expected to be undefined.

--- test/transactionRunner.js
+++ test/transactionRunner.js
@@ -1,6 +1,7 @@
 const Tx = require('../index.js')
 const tape = require('tape')
 const ethUtil = require('ethereumjs-util')
+const argv = require('minimist')(process.argv.slice(2))
 const testing = require('ethereumjs-testing')
 
 const forkNames = [
@@ -9,7 +10,7 @@ const forkNames = [
   'EIP150',
   'EIP158',
   'Frontier',
-  'Homestead'
+  'Homestead',
 ]
 
 tape('TransactionTests', (t) => {
@@ -17,34 +18,27 @@ tape('TransactionTests', (t) => {
     let rawTx
     let tx
     t.test(testName, (st) => {
-      try {
-        rawTx = ethUtil.toBuffer(testData.rlp)
-        tx = new Tx(rawTx)
-        if (Object.keys(testData.Homestead).length === 0) {
-          tx._homestead = false
-        }
-      } catch (e) {
-        st.equal(undefined, tx, 'should not have any fields ')
-        st.end()
-      }
-
+      const rawTx = ethUtil.toBuffer(testData.rlp)
+      let tx
       let sender
       let hash
-
-      if (tx && tx.validate()) {
-        sender = tx.getSenderAddress().toString('hex')
-        hash = tx.hash().toString('hex')
+      forkNames.forEach(forkName => {
         try {
-          forkNames.forEach(forkName => {
-            st.equal(testData[forkName].sender, sender)
-            st.equal(testData[forkName].hash, hash)
-          })
+          tx = new Tx(rawTx)
+          if (forkName !== 'Homestead') {
+            tx._homestead = false
+          }
+          const sender = tx.getSenderAddress().toString('hex')
+          const hash = tx.hash().toString('hex')
+          st.assert(sender === testData[forkName].sender && hash === testData[forkName].hash, `Sender and hash should be correct on ${forkName}`)
         } catch (e) {
-          st.fail(e)
+          if (tx !== undefined) {
+            st.equal(e.message, 'Invalid Signature', `Transaction signature should be invalid on ${forkName}`)
+          } else {
+            st.pass(`Transaction should be invalid and should error on creation on ${forkName}`)
+          }
         }
-      } else {
-        st.equal(undefined, tx, 'no tx params in test')
-      }
+      })
       st.end()
     })
   })
